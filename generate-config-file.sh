d35dede246f401b10414544a9db05945f023d979
zhu.rong <zhu.rong@99cloud.net>
2015-10-22 14:57:21 +0800

    Use config file for oslo-config-generator instead of generate-config-file.sh
    We don't need a separate script to wrap the oslo-config-generator. Like
    other projects, we can just specify a config-generator config file to
    define the namespaces.
    
    Change-Id: I20365d058fde9d388ca2a54e1b3854d99ef9f93b

--- generate-config-file.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-mkdir -p etc/evoque
-oslo-config-generator --output-file etc/evoque/evoque.conf \
-                      --namespace evoque \
-                      --namespace oslo.log \
